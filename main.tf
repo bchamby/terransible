fe8260b3bd872cea23409863d680ed7769f13ba0
Brad Chamberlain <chamby@gmail.com>
2017-04-14 10:42:46 -0400

    changed aws_iam_instance_profile to role with single value, list is deprecated in 0.9.3

--- main.tf
+++ main.tf
@@ -36,60 +36,60 @@ resource "aws_default_route_table" "private" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.1.1.0/24"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1d"
+  availability_zone       = "us-east-1d"
   tags {
     Name = "public"
   }
 }
 
 resource "aws_subnet" "private1" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.1.2.0/24"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.1.2.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
   tags {
     Name = "private1"
   }
 }
 
 resource "aws_subnet" "private2" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.1.3.0/24"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.1.3.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "us-east-1c"
+  availability_zone       = "us-east-1c"
   tags {
     Name = "private2"
   }
 }
 
 resource "aws_subnet" "rds1" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.1.4.0/24"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.1.4.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
   tags {
     Name = "rds1"
   }
 }
 
 resource "aws_subnet" "rds2" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.1.5.0/24"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.1.5.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "us-east-1c"
+  availability_zone       = "us-east-1c"
   tags {
     Name = "rds2"
   }
 }
 
 resource "aws_subnet" "rds3" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "10.1.6.0/24"
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.1.6.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "us-east-1d"
+  availability_zone       = "us-east-1d"
   tags {
     Name = "rds3"
   }
@@ -98,22 +98,22 @@ resource "aws_subnet" "rds3" {
 # subnet associations
 
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id = "${aws_subnet.public.id}"
+  subnet_id      = "${aws_subnet.public.id}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
 resource "aws_route_table_association" "private1_assoc" {
-  subnet_id = "${aws_subnet.private1.id}"
+  subnet_id      = "${aws_subnet.private1.id}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
 resource "aws_route_table_association" "private2_assoc" {
-  subnet_id = "${aws_subnet.private2.id}"
+  subnet_id      = "${aws_subnet.private2.id}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
 resource "aws_db_subnet_group" "rds_subnetgroup" {
-  name = "rds_subnetgroup"
+  name       = "rds_subnetgroup"
   subnet_ids = ["${aws_subnet.rds1.id}", "${aws_subnet.rds2.id}", "${aws_subnet.rds3.id}"]
   tags {
     Name = "rds_sng"
@@ -121,87 +121,88 @@ resource "aws_db_subnet_group" "rds_subnetgroup" {
 }
 
 resource "aws_security_group" "public" {
-  name = "sg_public"
+  name        = "sg_public"
   description = "Used for load balancer."
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id      = "${aws_vpc.vpc.id}"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["${var.localip}/32"]
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "private" {
-  name = "sg_private"
+  name        = "sg_private"
   description = "Used for private instances."
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id      = "${aws_vpc.vpc.id}"
 
 # access from other security groups
 
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["10.1.0.0/16"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "rds" {
-  name = "sg_rds"
+  name        = "sg_rds"
   description = "Used for DB instances."
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id      = "${aws_vpc.vpc.id}"
 
   ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = ["${aws_security_group.public.id}", "${aws_security_group.private.id}"]
   }
 }
 
 resource "aws_db_instance" "db" {
-  allocated_storage = 10
-  engine = "mysql"
-  engine_version = "5.6.27"
-  instance_class = "${var.db_instance_class}"
-  name = "${var.dbname}"
-  username = "${var.dbuser}"
-  password = "${var.dbpassword}"
-  db_subnet_group_name = "${aws_db_subnet_group.rds_subnetgroup.name}"
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.6.27"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbname}"
+  username               = "${var.dbuser}"
+  password               = "${var.dbpassword}"
+  db_subnet_group_name   = "${aws_db_subnet_group.rds_subnetgroup.name}"
   vpc_security_group_ids = ["${aws_security_group.rds.id}"]
+  skip_final_snapshot    = true
 }
 
 resource "aws_iam_instance_profile" "s3_access" {
-  name = "s3_access"
-  roles = ["${aws_iam_role.s3_access.name}"]
+  name  = "s3_access"
+  role = "${aws_iam_role.s3_access.name}"
 }
 
 resource "aws_iam_role_policy" "s3_access_policy" {
-  name = "s3_access_policy"
-  role = "${aws_iam_role.s3_access.id}"
+  name   = "s3_access_policy"
+  role   = "${aws_iam_role.s3_access.id}"
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -217,7 +218,7 @@ EOF
 }
 
 resource "aws_iam_role" "s3_access" {
-  name = "s3_access"
+  name               = "s3_access"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -237,10 +238,10 @@ EOF
 
 # Create S3 VPC endpoint
 resource "aws_vpc_endpoint" "private-s3" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  service_name = "com.amazonaws.${var.aws_region}.s3"
+  vpc_id          = "${aws_vpc.vpc.id}"
+  service_name    = "com.amazonaws.${var.aws_region}.s3"
   route_table_ids = ["${aws_vpc.vpc.main_route_table_id}", "${aws_route_table.public.id}"]
-  policy = <<POLICY
+  policy          = <<POLICY
 {
   "Statement": [
     {
@@ -257,8 +258,8 @@ POLICY
 #S3 code bucket
 
 resource "aws_s3_bucket" "code" {
-  bucket = "${var.domain_name}_code1115"
-  acl = "private"
+  bucket        = "${var.domain_name}_code1115"
+  acl           = "private"
   force_destroy = true
   tags {
     Name = "code bucket"
@@ -266,7 +267,7 @@ resource "aws_s3_bucket" "code" {
 }
 
 resource "aws_key_pair" "auth" {
-  key_name = "${var.key_name}"
+  key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
 
@@ -274,14 +275,14 @@ resource "aws_key_pair" "auth" {
 
 resource "aws_instance" "dev" {
   instance_type = "${var.dev_instance_type}"
-  ami = "${var.dev_ami}"
+  ami           = "${var.dev_ami}"
   tags {
     Name = "dev"
   }
-  key_name = "${aws_key_pair.auth.id}"
+  key_name               = "${aws_key_pair.auth.id}"
   vpc_security_group_ids = ["${aws_security_group.public.id}"]
-  iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
-  subnet_id = "${aws_subnet.public.id}"
+  iam_instance_profile   = "${aws_iam_instance_profile.s3_access.id}"
+  subnet_id              = "${aws_subnet.public.id}"
 
   provisioner "local-exec" {
     command = <<EOP
@@ -300,28 +301,28 @@ EOP
 }
 
 resource "aws_elb" "prod" {
-  name = "${var.domain_name}-prod-elb"
-  subnets = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}"]
+  name            = "${var.domain_name}-prod-elb"
+  subnets         = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}"]
   security_groups = ["${aws_security_group.public.id}"]
 
   listener {
-    instance_port = 80
+    instance_port     = 80
     instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
 
   health_check {
-    healthy_threshold = "${var.elb_healthy_threshold}"
+    healthy_threshold   = "${var.elb_healthy_threshold}"
     unhealthy_threshold = "${var.elb_unhealthy_threshold}"
-    timeout = "${var.elb_timeout}"
-    target = "HTTP:80/"
-    interval = "${var.elb_interval}"
+    timeout             = "${var.elb_timeout}"
+    target              = "HTTP:80/"
+    interval            = "${var.elb_interval}"
   }
 
-  cross_zone_load_balancing = true
-  idle_timeout = 400
-  connection_draining = true
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
   connection_draining_timeout = 400
 
   tags {
@@ -334,7 +335,7 @@ resource "random_id" "ami" {
 }
 
 resource "aws_ami_from_instance" "golden" {
-  name = "ami-${random_id.ami.b64}"
+  name               = "ami-${random_id.ami.b64}"
   source_instance_id = "${aws_instance.dev.id}"
   provisioner "local-exec" {
     command = <<EOT
@@ -349,34 +350,34 @@ EOT
 }
 
 resource "aws_launch_configuration" "lc" {
-  name_prefix = "lc-"
-  image_id = "${aws_ami_from_instance.golden.id}"
-  instance_type = "${var.lc_instance_type}"
-  security_groups = ["${aws_security_group.private.id}"]
+  name_prefix          = "lc-"
+  image_id             = "${aws_ami_from_instance.golden.id}"
+  instance_type        = "${var.lc_instance_type}"
+  security_groups      = ["${aws_security_group.private.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
-  key_name = "${aws_key_pair.auth.id}"
-  user_data = "${file("userdata")}"
+  key_name             = "${aws_key_pair.auth.id}"
+  user_data            = "${file("userdata")}"
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_group" "asg" {
-  availability_zones = ["${var.aws_region}a", "${var.aws_region}c"]
-  name = "asg-${aws_launch_configuration.lc.id}"
-  max_size = "${var.asg_max}"
-  min_size = "${var.asg_min}"
+  availability_zones        = ["${var.aws_region}a", "${var.aws_region}c"]
+  name                      = "asg-${aws_launch_configuration.lc.id}"
+  max_size                  = "${var.asg_max}"
+  min_size                  = "${var.asg_min}"
   health_check_grace_period = "${var.asg_grace}"
-  health_check_type = "${var.asg_hct}"
-  desired_capacity = "${var.asg_cap}"
-  force_delete = true
-  load_balancers = ["${aws_elb.prod.id}"]
-  vpc_zone_identifier = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}"]
-  launch_configuration = "${aws_launch_configuration.lc.name}"
+  health_check_type         = "${var.asg_hct}"
+  desired_capacity          = "${var.asg_cap}"
+  force_delete              = true
+  load_balancers            = ["${aws_elb.prod.id}"]
+  vpc_zone_identifier       = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}"]
+  launch_configuration      = "${aws_launch_configuration.lc.name}"
 
   tag {
-    key = "Name"
-    value = "asg-instance"
+    key                 = "Name"
+    value               = "asg-instance"
     propagate_at_launch = true
   }
 
@@ -387,28 +388,28 @@ resource "aws_autoscaling_group" "asg" {
 
 resource "aws_route53_record" "www" {
   zone_id = "${var.route53_hosted_zone_id}"
-  name = "www.${var.domain_name}.io"
-  type = "A"
+  name    = "www.${var.domain_name}.io"
+  type    = "A"
 
   alias {
-    name = "${aws_elb.prod.dns_name}"
-    zone_id = "${aws_elb.prod.zone_id}"
+    name                   = "${aws_elb.prod.dns_name}"
+    zone_id                = "${aws_elb.prod.zone_id}"
     evaluate_target_health = false
   }
 }
 
 resource "aws_route53_record" "dev" {
   zone_id = "${var.route53_hosted_zone_id}"
-  name = "dev.${var.domain_name}.io"
-  type = "A"
-  ttl = "300"
+  name    = "dev.${var.domain_name}.io"
+  type    = "A"
+  ttl     = "300"
   records = ["${aws_instance.dev.public_ip}"]
 }
 
 resource "aws_route53_record" "db" {
   zone_id = "${var.route53_hosted_zone_id}"
-  name = "db.${var.domain_name}.io"
-  type = "CNAME"
-  ttl = "300"
+  name    = "db.${var.domain_name}.io"
+  type    = "CNAME"
+  ttl     = "300"
   records = ["${aws_db_instance.db.address}"]
 }
